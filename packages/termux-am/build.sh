a3b765265561f98fe0fa09744e5e5cf2d25dd9ed
Prodysor <278162675+Prodysor@users.noreply.github.com>
2026-09-06 15:00:36 +0200

    build(termux-am): align Android Gradle toolchain

--- packages/termux-am/build.sh
+++ packages/termux-am/build.sh
@@ -10,7 +10,7 @@ TERMUX_PKG_SHA256=7d4cfa2bfff93d5fc89fc89e537d2c072e08918276b140b7ed48ea45ebfbe8
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFLICTS="termux-tools (<< 0.51)"
-_GRADLE_VERSION=8.10.2
+_GRADLE_VERSION=9.2.1
 
 termux_step_post_get_source() {
 	sed -i'' -E -e "s|\@TERMUX_PREFIX\@|${TERMUX_PREFIX}|g" "$TERMUX_PKG_SRCDIR/am-libexec-packaged"
@@ -21,7 +21,9 @@ termux_step_post_get_source() {
 	sed -i'' -E \
 		-e "s|compileSdkVersion 33|compileSdkVersion 35\n    buildToolsVersion \"${TERMUX_ANDROID_BUILD_TOOLS_VERSION}\"|" \
 		"$TERMUX_PKG_SRCDIR/app/build.gradle"
-	echo "android.suppressUnsupportedCompileSdk=35" >> "$TERMUX_PKG_SRCDIR/gradle.properties"
+	sed -i'' -E \
+		-e "s|com.android.tools.build:gradle:7.4.2|com.android.tools.build:gradle:8.13.2|" \
+		"$TERMUX_PKG_SRCDIR/build.gradle"
 }
 
 termux_step_make() {
@@ -29,7 +31,7 @@ termux_step_make() {
 	termux_download \
 		https://services.gradle.org/distributions/gradle-$_GRADLE_VERSION-bin.zip \
 		$TERMUX_PKG_CACHEDIR/gradle-$_GRADLE_VERSION-bin.zip \
-		31c55713e40233a8303827ceb42ca48a47267a0ad4bab9177123121e71524c26
+		72f44c9f8ebcb1af43838f45ee5c4aa9c5444898b3468ab3f4af7b6076c5bc3f
 	mkdir $TERMUX_PKG_TMPDIR/gradle
 	unzip -q $TERMUX_PKG_CACHEDIR/gradle-$_GRADLE_VERSION-bin.zip -d $TERMUX_PKG_TMPDIR/gradle
 
